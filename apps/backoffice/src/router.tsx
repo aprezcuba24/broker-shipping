b58e96c6e00d8841396efb9254e7454aa42e6c71
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2026-05-25 14:39:24 -0400

    fix cors

--- apps/backoffice/src/router.tsx
+++ apps/backoffice/src/router.tsx
@@ -1,5 +1,5 @@
 import { RequireAuth } from '@broker/api'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { BackofficeLayout } from './layouts/backoffice-layout'
 import { LoginPage } from './pages/login'
 import { ProductListPage } from './pages/product/list'
@@ -16,8 +16,10 @@ export default function App() {
             </RequireAuth>
           }
         >
+          <Route index element={<Navigate to="/products" replace />} />
           <Route path="/products" element={<ProductListPage />} />
         </Route>
+        <Route path="*" element={<Navigate to="/products" replace />} />
       </Routes>
     </BrowserRouter>
   )
